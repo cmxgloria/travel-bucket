e7cb562e016c195202cb28da7bd8a88b7fea66a4
Gloria Chen <cmxgloria@gmail.com>
2020-02-21 15:38:30 +1100

    updated

--- schema.sql
+++ schema.sql
@@ -30,7 +30,6 @@ CREATE TABLE activity(
 
 
 
--- bucket seeding
 INSERT INTO bucket (city, city_photo,user_id) VALUES('MEl','http://test1',1);
 INSERT INTO bucket (city, city_photo,user_id) VALUES('MEl2','http://test2',1);
 INSERT INTO bucket (city, city_photo,user_id) VALUES('MEl3','http://test1',1);
@@ -43,19 +42,23 @@ INSERT INTO bucket (city, city_photo,user_id) VALUES('Melbourne','https://media0
 INSERT INTO bucket (city, city_photo, user_id, budget, year, month) VALUES('FrankFurk','https://media0.giphy.com/media/5BYt33NDLURd1Oq8UN/giphy-downsized.gif?cid=44b7432c30f47bbd2d4e61898c5ddbb97ae411b4f7154a69&rid=giphy-downsized.gif',1,5000,2022,7);
 
 
--- activity seeding
 INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('run','run all day long','http://act1',1);
 INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('drive','drive all day long','http://act2',1);
 INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('drive','drive all day long','http://act1',2);
 INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('sleep','sleep all day long','http://act1',2);
 INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('run','run all day long','http://act1',3);
 INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('run','run all day long','http://act1',4);
-INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('ski','ski all day long','https://media0.giphy.com/media/l2JHY6zkjwNRmNiGk/giphy.gif?cid=44b7432c48d49e603cedbbe4e638b695b6e9948503d38624&rid=giphy.gif',2);
-INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('run','run all day long','https://media2.giphy.com/media/7kn27lnYSAE9O/giphy.gif?cid=44b7432cf3fc3fed0cde492f48a8f8d5c44e5f98c3d85d52&rid=giphy.gif',2);
-INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('Swim','swim all day long','https://media0.giphy.com/media/kQ5laHax6S35K/giphy-downsized.gif?cid=44b7432ca21ee638ead15d005e58925cbeadb86d21d5e986&rid=giphy-downsized.gif',2);
-INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('Eat','eat anything all day long','https://media1.giphy.com/media/xTiTnsU6IKXbH0atkA/giphy-downsized.gif?cid=44b7432cb478f751bc65faa5726650ae12f11a2c852e7106&rid=giphy-downsized.gif',2);
+INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('ski','ski all day long','https://media0.giphy.com/media/l2JHY6zkjwNRmNiGk/giphy.gif?cid=44b7432c48d49e603cedbbe4e638b695b6e9948503d38624&rid=giphy.gif',3);
+INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('run','run all day long','https://media2.giphy.com/media/7kn27lnYSAE9O/giphy.gif?cid=44b7432cf3fc3fed0cde492f48a8f8d5c44e5f98c3d85d52&rid=giphy.gif',3);
+INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('Swim','swim all day long','https://media0.giphy.com/media/kQ5laHax6S35K/giphy-downsized.gif?cid=44b7432ca21ee638ead15d005e58925cbeadb86d21d5e986&rid=giphy-downsized.gif',4);
+INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('Eat','eat anything all day long','https://media1.giphy.com/media/xTiTnsU6IKXbH0atkA/giphy-downsized.gif?cid=44b7432cb478f751bc65faa5726650ae12f11a2c852e7106&rid=giphy-downsized.gif',4);
+INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('ski','ski all day long','https://media0.giphy.com/media/l2JHY6zkjwNRmNiGk/giphy.gif?cid=44b7432c48d49e603cedbbe4e638b695b6e9948503d38624&rid=giphy.gif',4);
+INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('run','run all day long','https://media2.giphy.com/media/7kn27lnYSAE9O/giphy.gif?cid=44b7432cf3fc3fed0cde492f48a8f8d5c44e5f98c3d85d52&rid=giphy.gif',4);
+INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('Swim','swim all day long','https://media0.giphy.com/media/kQ5laHax6S35K/giphy-downsized.gif?cid=44b7432ca21ee638ead15d005e58925cbeadb86d21d5e986&rid=giphy-downsized.gif',3);
+INSERT INTO activity (activity_title,activity_detail,activity_photo,bucket_id) VALUES('Eat','eat anything all day long','https://media1.giphy.com/media/xTiTnsU6IKXbH0atkA/giphy-downsized.gif?cid=44b7432cb478f751bc65faa5726650ae12f11a2c852e7106&rid=giphy-downsized.gif',3);
 
---user seeding
 INSERT INTO users (username, github_id) VALUES('jp1',12423423);
 INSERT INTO users (username, github_id) VALUES('jp2',12423423);
 INSERT INTO users (username, github_id) VALUES('sriramank1988',43689259);
+INSERT INTO users (username, github_id) VALUES('cmxgloria',47142507);
+
